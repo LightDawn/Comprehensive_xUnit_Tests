fd6235f84fe906761ea01d2054583c6645c6cec9
behi <behnam.alavi@gmail.com>
2023-12-02 20:56:53 +0330

    Some changes.

--- FactTests/obj/Debug/net8.0/FactTests.AssemblyInfo.cs
+++ FactTests/obj/Debug/net8.0/FactTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FactTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95df5bf587f7ef0d26325cfd9cc2575673e33ba7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FactTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FactTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
